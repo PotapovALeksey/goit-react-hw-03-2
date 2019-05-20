7d687c88f0aa9fea2ca823e51791e3ff75dc68f6
Potapov Aleksey <alex.potap1811@gmail.com>
2019-05-21 00:01:15 +0300

    Updates

---
+++ precache-manifest.b12ac6db96d133f2ab9b8dabd364deed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518d11dc0701f344ce6b2c5fcfb97349",
+    "url": "/goit-react-hw-03-2/index.html"
+  },
+  {
+    "revision": "e1a17fa81dbe97d4b170",
+    "url": "/goit-react-hw-03-2/static/css/2.b672ec7a.chunk.css"
+  },
+  {
+    "revision": "fecddec7e5f87d453f31",
+    "url": "/goit-react-hw-03-2/static/css/main.f59e08ad.chunk.css"
+  },
+  {
+    "revision": "e1a17fa81dbe97d4b170",
+    "url": "/goit-react-hw-03-2/static/js/2.5f7490da.chunk.js"
+  },
+  {
+    "revision": "fecddec7e5f87d453f31",
+    "url": "/goit-react-hw-03-2/static/js/main.858b1bab.chunk.js"
+  },
+  {
+    "revision": "fd67dc772ef10ed479b2",
+    "url": "/goit-react-hw-03-2/static/js/runtime~main.43da7b74.js"
+  }
+]);
